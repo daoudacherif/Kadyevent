17e2ce140fff16ce93a8a9fc2893a58f911f88ab
daoudacherif <daoudacherif4321@gmail.com>
2025-03-27 13:06:29 +0000

    add to repo

---
+++ includes/headercat.php
@@ -0,0 +1,233 @@
+
+
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Christ - Christmas Gift Shop HTML Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet"> 
+    <!-- CSS
+	============================================ -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/ionicons.min.css">
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="../assets/css/plugins.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="../style.css">
+    <!-- Modernizer JS -->
+    <script src="./assets/jsvendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+
+<!-- Main Wrapper Start -->
+<div id="main-wrapper" class="section">
+    <!-- Header Section Start -->
+    <div class="header-section section">
+       
+        <!-- Header Top Start -->
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+
+                        <!-- Header Top Wrapper Start -->
+                        <div class="header-top-wrapper">
+                            <div class="row">
+
+                                <!-- Header Social -->
+                                <div class="header-social col-md-4 col-12">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                    <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                                </div>
+
+                                <!-- Header Logo -->
+                                <div class="header-logo col-md-4 col-12">
+                                    <a href="index.php" class="logo"><img src="../assets/img/logo.png" alt="logo" style="height: 120px; width: 200px;"></a>
+                                </div>
+
+                                <!-- Account Menu -->
+                                <div class="account-menu col-md-4 col-12">
+                                    <ul>
+                                        <li><a href="../checkout.php">connect</a></li>
+                                        <li><a href="../logout.php">Disconnect</a></li>
+                                        <li><a href="../wishlist.php">Wishlist</a></li>
+                                        <li><a href="#" data-toggle="dropdown"><i class="fa fa-shopping-cart"></i><span class="num">2</span></a>
+                                            
+                                            <!-- Mini Cart -->
+                                            <div class="mini-cart-brief dropdown-menu text-left">
+                                                <!-- Cart Products -->
+                                                <div class="all-cart-product clearfix">
+                                                    <div class="single-cart clearfix">
+                                                        <div class="cart-image">
+                                                            <a href="product-details.php"><img src="../assets/img/cart/1.jpg" alt=""></a>
+                                                        </div>
+                                                        <div class="cart-info">
+                                                            <h5><a href="product-details.php">Holiday Candle</a></h5>
+                                                            <p>1 x £9.00</p>
+                                                            <a href="#" class="cart-delete" title="Remove this item"><i class="fa fa-trash-o"></i></a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="single-cart clearfix">
+                                                        <div class="cart-image">
+                                                            <a href="product-details.php"><img src="../assets/img/cart/2.jpg" alt=""></a>
+                                                        </div>
+                                                        <div class="cart-info">
+                                                            <h5><a href="product-details.php">Christmas Tree</a></h5>
+                                                            <p>1 x £9.00</p>
+                                                            <a href="#" class="cart-delete" title="Remove this item"><i class="fa fa-trash-o"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- Cart Total -->
+                                                <div class="cart-totals">
+                                                    <h5>Total <span>£12.00</span></h5>
+                                                </div>
+                                                <!-- Cart Button -->
+                                                <div class="cart-bottom  clearfix">
+                                                    <a href="../checkout.php">Check out</a>
+                                                </div>
+                                            </div>
+                                            
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div><!-- Header Top Wrapper End -->
+
+                    </div>
+                </div>
+            </div>
+        </div><!-- Header Top End -->
+        
+        <!-- Header Bottom Start -->
+        <div class="header-bottom section">
+            <div class="container">
+                <div class="row">
+                   
+                    <!-- Header Bottom Wrapper Start -->
+                    <div class="header-bottom-wrapper text-center col">
+
+                        <!-- Header Bottom Logo -->
+                        <div class="header-bottom-logo">
+                            <a href="index.php" class="logo"><img src="../assets/img/logo.png" alt="logo"></a>
+                        </div>
+
+                        <!-- Main Menu -->
+                        <nav id="main-menu" class="main-menu">
+                            <ul>
+                                <li class="active"><a href="../index.php">home</a></li>
+                                <li><a href="">Categories</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="deco.php">Decoration</a></li>
+                                        <li><a href="cake.php">Cake designer</a></li>
+                                        <li><a href="salle.php">salle</a></li>
+                                        <li><a href="makeup.php">Maquillage</a></li>
+                                        <li><a href="hair.php">coiffure</a></li>
+                                        <li><a href="photo.php">photographe</a></li>
+                                        
+                                      
+                                    </ul>
+                                </li>
+                                <li><a href="../about.php">about</a></li>
+                                <li><a href="../wishlist.php">wishlist</a></li>
+                                <li><a href="../contact.php">contact</a></li>
+                            </ul>
+                        </nav>
+                        
+                        <!-- Header Search -->
+                        <div class="header-search">
+                            
+                            <!-- Search Toggle -->
+                            <button class="search-toggle"><i class="ion-ios-search-strong"></i></button>
+                            
+                            <!-- Search Form -->
+                            <div class="header-search-form">
+                                <form action="#">
+                                    <input type="text" placeholder="Search...">
+                                    <button><i class="ion-ios-search-strong"></i></button>
+                                </form>
+                            </div>
+                            
+                        </div>
+                        
+                        <!-- Mobile Menu -->
+                        <div class="mobile-menu section d-md-none"></div>
+
+                    </div><!-- Header Bottom Wrapper End -->
+                    
+                </div>
+            </div>
+        </div><!-- Header Bottom End -->
+        
+    </div><!-- Header Section End -->
+    
+       
+    <!-- Hero Slider Start-->
+    <div class="hero-slider section fix">
+
+        <!-- Hero Slide Item Start-->
+        <div class="hero-item" style="background-image: url(../assets/img/hero/1.jpg)">
+
+            <!-- Hero Content Start-->
+            <div class="hero-content text-center m-auto">
+
+                <h2>Save 25%</h2>
+                <h1>Christmas Sale</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which.</p>
+                <a href="#">LEARN MORE</a>
+
+            </div><!-- Hero Content End-->
+
+
+        </div><!-- Hero Slide Item End-->
+
+        <!-- Hero Slide Item Start-->
+        <div class="hero-item" style="background-image: url(../assets/img/hero/2.jpg)">
+
+            <!-- Hero Content Start-->
+            <div class="hero-content text-center m-auto">
+
+                <h2>Save 25%</h2>
+                <h1>Christmas Sale</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which.</p>
+                <a href="#">LEARN MORE</a>
+
+            </div><!-- Hero Content End-->
+
+
+        </div><!-- Hero Slide Item End-->
+
+    </div><!-- Hero Slider End-->
+
+    <!-- JS
+============================================ -->
+
+<!-- jQuery JS -->
+<script src="../assets/js/vendor/jquery-1.12.0.min.js"></script>
+<!-- Popper JS -->
+<script src="../assets/jspopper.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="../assets/jsbootstrap.min.js"></script>
+<!-- Plugins JS -->
+<script src="../assets/jsplugins.js"></script>
+<!-- Ajax Mail JS -->
+<script src="../assets/jsajax-mail.js"></script>
+<!-- Main JS -->
+<script src="../assets/jsmain.js"></script>
+</body>
